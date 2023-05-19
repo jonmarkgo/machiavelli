77db6b0258d30936b6016c5a2771645d4d306b1b
Mario Benito <mario@maberi.com>
2023-05-19 13:37:47 +0200

    Upgrade to Django 4. Changing uget* to get

--- signals/handlers.py
+++ signals/handlers.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.utils.translation import ugettext_noop as _
+from django.utils.translation import gettext_noop as _
+
 
 def create_notice_types(sender, **kwargs):
     if "pinax.notifications" in settings.INSTALLED_APPS:
